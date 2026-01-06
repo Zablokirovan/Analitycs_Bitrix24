d2c4bb78fd708cdcd567252bed150dfa4d6f6f73
Zablokirovan <artemiszbobylev05@gmail.com>
2026-01-06 16:04:55 +0500

    add function for upload data in database

---
+++ Database.py
@@ -0,0 +1,33 @@
+"""
+This file need for upload data in database Clickhouse
+"""
+import os
+import clickhouse_connect
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = clickhouse_connect.get_client(
+    host=os.getenv('DB_HOST'),
+    database=os.getenv('DB_DATABASE'),
+    username=os.getenv('DB_USERNAME'),
+    password=os.getenv('DB_PASSWORD'),
+    port=int(os.getenv('DB_PORT')),
+    #connect in https, not http
+    secure=True,
+    #Disables server SSL certificate verification.
+    verify=False
+)
+
+def upload_db_deals_create(deal_list):
+    """
+    Loading transaction data exported by creation date into the database
+    :param deal_list: List
+    :return: none
+    """
+    table = 'bitrix.deals_created'
+    columns=['deal_id', 'contact_id','stage_id', 'close_date', 'date_create',
+             'date_modify', 'category_id', 'source_id', 'semantic_id',
+             'created_by_id', 'blank_url', 'department_id' ]
+    client.insert(table=table, column_names=columns, data=deal_list)
+
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import Bitrix24
+import Database
 
-
+#deal_id_dc = deal_id
+#deal_dc_modify_data = valid data for upload in database
 deal_id_dc, deal_dc_modify_data = Bitrix24.get_deals_date_create()
-#modify date for deals upload in dete create
+Database.upload_db_deals_create(deal_dc_modify_data)
 
-print(deal_id_dc,deal_dc_modify_data)
 
 
 
