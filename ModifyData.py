62fc13b004418a03311b028483e273d48d504018
Zablokirovan <artemiszbobylev05@gmail.com>
2026-01-06 18:09:16 +0500

    Flake8

--- ModifyData.py
+++ ModifyData.py
@@ -3,12 +3,13 @@ This file is required to modify the data downloaded from Bitrix24.
  For further loading into the Database
 """
 
-from datetime import datetime, date
+from datetime import datetime
+
 
 def data_modify_for_deals(deal_list, with_version: bool = False):
-    #deal_ids
-    deal_id=[]
-    #valid data for deals
+    # deal_ids
+    deal_id = []
+    # valid data for deals
     date_for_upload = []
 
     load_at = datetime.now()
@@ -18,21 +19,22 @@ def data_modify_for_deals(deal_list, with_version: bool = False):
         deal_id.append(deal['ID'])
 
         row = [int(deal['ID']),
-            int(deal['CONTACT_ID']) if deal.get('CONTACT_ID') else None,
-            deal['STAGE_ID'],
-            datetime.fromisoformat(deal['CLOSEDATE']).date(),
-            datetime.fromisoformat(deal['DATE_CREATE']).date(),
-            datetime.fromisoformat(deal['DATE_MODIFY']).date(),
-            int(deal['CATEGORY_ID']),
-            deal['SOURCE_ID'],
-            deal['STAGE_SEMANTIC_ID'],
-            int(deal['CREATED_BY_ID']) if deal.get('CREATED_BY_ID') else None,
-            None if deal['UF_CRM_1569388305'] is None else
-            deal['UF_CRM_1569388305']['downloadUrl'],
-            int(deal['UF_CRM_5F3F5BECDFC07']) if deal.get(
-                'UF_CRM_5F3F5BECDFC07'
-            ) else None]
-        #Since the same error handling logic is used for deals loaded by
+               int(deal['CONTACT_ID']) if deal.get('CONTACT_ID') else None,
+               deal['STAGE_ID'],
+               datetime.fromisoformat(deal['CLOSEDATE']).date(),
+               datetime.fromisoformat(deal['DATE_CREATE']).date(),
+               datetime.fromisoformat(deal['DATE_MODIFY']).date(),
+               int(deal['CATEGORY_ID']),
+               deal['SOURCE_ID'],
+               deal['STAGE_SEMANTIC_ID'],
+               int(deal['CREATED_BY_ID']) if
+               deal.get('CREATED_BY_ID') else None,
+               None if deal['UF_CRM_1569388305'] is None else
+               deal['UF_CRM_1569388305']['downloadUrl'],
+               int(deal['UF_CRM_5F3F5BECDFC07']) if deal.get(
+                   'UF_CRM_5F3F5BECDFC07'
+               ) else None]
+        # Since the same error handling logic is used for deals loaded by
         # creation date and date modified, for deals loaded by creation date,
         # the with_version flag will be set to true, and a version record will
         # be added to the shared array. For deals loaded by date modified only
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import Bitrix24
 import Database
 
-#deal_id_dc = deal_id
-#deal_dc_modify_data = valid data for upload in database
+
+# deal_id_dc = deal_id
+# deal_dc_modify_data = valid data for upload in database
 def get_and_upload_deals_create():
     deal_id_dc, deal_dc_modify_data = Bitrix24.get_deals_date_create()
     Database.upload_db_deals_create(deal_dc_modify_data)
+
 
 def get_and_upload_deal_modify():
     deal_id_dm, deal_dm_modify_data = Bitrix24.get_deals_date_modify()
     Database.upload_db_deals_modify(deal_dm_modify_data)
     return deal_id_dm
-
-
 
 
 if __name__ == "__main__":
