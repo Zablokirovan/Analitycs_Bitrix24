7c6f675a536e0c09d43c73da009467266c579808
Zablokirovan <artemiszbobylev05@gmail.com>
2026-01-20 16:12:39 +0500

    add comments

--- Config.py
+++ Config.py
@@ -1,25 +1,27 @@
 """
 The file is needed to change the constant that works during unloading.
 """
+
+# Shema in database
 shema = 'bitrix'
 
 # Table name for deals loaded by creation date
 table_deal_create = 'deals_by_date_create'
 
-#
+# Table name for deals loaded by modify date
 table_deal_modify = 'deals_by_date_modify'
 
-#
+# Table name for transaction history
 table_deal_history = 'deal_history_stage'
 
-#
+# Table name for all departmet in Bitrix24
 table_department = 'bitrix_department'
 
-#
+# Table name for users in Bitrix24
 table_user = 'bitrix_user'
 
-
+# Table name for all category in Bitrix24
 table_category = 'table_category'
 
-
+# Table name for name stage in category
 table_category_stage = 'bitrix_category_stage'
